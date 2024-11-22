1e8fc09c966a8304c898c4561f3f3f29857498b0
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-11-22 22:32:19 +0530

    Update Move All Zeroes to End.java

--- Move All Zeroes to End.java
+++ Move All Zeroes to End.java
@@ -1,6 +1,7 @@
 /*
 Given an array arr[]. Push all the zeros of the given array to the right end of the array while maintaining the order of non-zero elements. Do the mentioned change in the array in place.
 */
+//------------------- Aproach 01 -----------------------//
 class Solution {
     void pushZerosToEnd(int[] arr) {
         // code here
@@ -13,5 +14,21 @@ class Solution {
                 left++;
             }
         }
+    }
+}
+//------------------- Aproach 02 -----------------------//
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != 0){
+                arr[count++] = arr[i];
+            }
+        }
+        while(count < arr.length){
+            arr[count++] = 0;
+        }
+        
     }
 }
