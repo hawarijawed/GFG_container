054f547bfea1dd38c67c2dfff9d97a40a95ccb5a
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-12-13 21:43:44 +0530

    Return the minimum from the sorted and rotated array

---
+++ Sorted and Rotated Minimum.java
@@ -0,0 +1,24 @@
+/*
+A sorted array of distinct elements arr[] is rotated at some unknown point, the task is to find the minimum element in it. 
+*/
+class Solution {
+    public int findMin(int[] arr) {
+        // complete the function here
+        int n = arr.length, st = 0, en = n - 1;
+        
+        while (st < en) {
+            if (arr[st] < arr[en])        
+                return arr[st];
+               
+            int mid = st + (en - st) / 2;
+            
+            if (arr[en] < arr[mid]) {
+                st = mid + 1;
+            } else {
+                en = mid;
+            }
+        }
+        
+        return arr[st];
+    }
+}
