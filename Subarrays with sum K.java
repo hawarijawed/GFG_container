4c4202c27e181672b80037c56ebf8d8821c91785
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-01-02 22:41:46 +0530

    find the number of continuous subarrays having sum exactly equal to a given number k

---
+++ Subarrays with sum K.java
@@ -0,0 +1,23 @@
+/*
+Given an unsorted array of integers, find the number of continuous subarrays having sum exactly equal to a given number k.
+*/
+class Solution {
+    public int countSubarrays(int arr[], int k) {
+        // code here
+        int sum = 0;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for(int i = 0; i<arr.length; i++) {
+            sum += arr[i];
+            int rem = sum - k;
+
+            if(map.containsKey(rem)) {
+                count += map.get(rem);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+        return count;
+    }
+}
