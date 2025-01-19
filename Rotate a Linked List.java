6b111b87586815a92c20345622b758ca5741b677
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-01-19 21:09:45 +0530

    Rotate the linked list k times in left direction

---
+++ Rotate a Linked List.java
@@ -0,0 +1,27 @@
+/*
+Given the head of a singly linked list, your task is to left rotate the linked list k times.
+*/
+class Solution {
+    public Node rotate(Node head, int k) {
+        // add code here
+        int size=0;
+        Node head1=head;
+        Node prev=null;
+        while(head1!=null){
+            prev=head1;
+            head1=head1.next;
+            size++;
+        }
+        k=k%size;
+        while(k-->0){
+            Node temp=head.next;
+            prev.next=head;
+            prev=prev.next;
+            head.next=null;
+            head=temp;
+            
+        }
+        return head;
+          
+    }
+}
