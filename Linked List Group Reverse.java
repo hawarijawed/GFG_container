b7f90360a5a73dc03e81446e82f33e9a06db26ba
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-01-21 18:14:40 +0530

    Reverse every k node in the linked list

---
+++ Linked List Group Reverse.java
@@ -0,0 +1,37 @@
+/*
+Given the head a linked list, the task is to reverse every k node in the linked list. If the number of nodes is not a multiple of k then the left-out nodes in the end, should be considered as a group and must be reversed.
+*/
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    public static Node reverseKGroup(Node head, int k) {
+        // code here
+        if(head == null) return head;
+        Node curr = head;
+        Node prev = null;
+        Node next = curr.next;
+        int count=0;
+        while(curr != null && count < k){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        
+        if(next != null) head.next = reverseKGroup(next, k);
+        return prev;
+    }
+}
