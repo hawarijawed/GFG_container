2e48694929beab1d7c3da25e687fe3e4c2a68008
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-02-01 20:41:20 +0530

    Check the existence of given word in matrix

---
+++ Word Search.java
@@ -0,0 +1,40 @@
+/*
+You are given a two-dimensional mat[][] of size n*m containing English alphabets and a string word. Check if the word exists on the mat. The word can be constructed by using letters from adjacent cells, either horizontally or vertically. 
+The same cell cannot be used more than once.
+*/
+class Solution {
+    static boolean checkIfExists(char[][] mat,int i, int j, String word, int idx){
+        if(idx == word.length()){
+            return true;
+        }
+        if(i<0 || j<0 || i>=mat.length || j>=mat[0].length){
+            return false;
+        }
+        if(mat[i][j] == word.charAt(idx)){
+            mat[i][j] = '-';
+           boolean ans = checkIfExists(mat, i+1, j, word, idx+1) || 
+           checkIfExists(mat, i-1, j, word, idx+1) ||
+           checkIfExists(mat, i, j+1, word, idx+1) ||
+           checkIfExists(mat, i, j-1, word, idx+1); 
+           mat[i][j] = word.charAt(idx);
+           return ans;
+           
+        } else {
+            return false;
+        }
+    }
+    
+    static public boolean isWordExist(char[][] mat, String word) {
+        // Code here
+        for(int i=0;i<mat.length;i++){
+            for(int j=0; j<mat[i].length; j++){
+                if(mat[i][j]==word.charAt(0)){
+                    if(checkIfExists(mat, i, j, word, 0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
