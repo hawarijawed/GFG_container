766b2240bf80848e2aa9032d51bbf0bfa9bd4038
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2025-11-12 22:57:22 +0545

    Given two strings pat and txt which may be of different sizes, You have to return true if the wildcard pattern i.e. pat, matches with txt else return false.

---
+++ Wildcard Pattern Matching.java
@@ -0,0 +1,39 @@
+/*
+Given two strings pat and txt which may be of different sizes, You have to return true if the wildcard pattern i.e. pat, matches with txt else return false.
+
+The wildcard pattern pat can include the characters '?' and '*'.
+
+'?' – matches any single character.
+'*' – matches any sequence of characters (including the empty sequence).
+Note: The matching should cover the entire txt (not partial txt).
+*/
+class Solution {
+    public boolean wildCard(String txt, String pat) {
+        // code here
+        int n = txt.length();
+        int m = pat.length();
+        
+        boolean [][]dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for(int i = 1; i <= m; i++) {
+            if(pat.charAt(i - 1) != '*') break;
+            dp[0][i] = dp[0][i - 1];
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            char s = txt.charAt(i - 1);
+            for(int j = 1; j <= m; j++) {
+                char t = pat.charAt(j - 1);
+                
+                if(s == t || t == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(t == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
